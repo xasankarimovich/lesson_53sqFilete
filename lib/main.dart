8846be2ca5bdbc2e0471d1aac1809aba03c44c6d
Karimovich. <146013797+xasankarimovich@users.noreply.github.com>
2024-06-09 23:51:12 -0700

    first

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:lesson_53/screens/home_screen.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen(),
+    );
+  }
+}
